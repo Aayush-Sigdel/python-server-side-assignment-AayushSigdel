0da64207fe17246d84cf20aa31e9b18e08e489f1
Aayush-Sigdel <itsforaayush@gmail.com>
2025-05-30 08:24:08 +0545

    task3

---
+++ task_2.py
@@ -0,0 +1,23 @@
+﻿student = []
+
+print("Enter the student's Details")
+n = int(input("Enter the Total number of students: "))
+for i in range(n):
+    print('#' * 75)
+    print("Enter Student Number " + str(i+1))
+    print('#' * 75)
+    name = input("Enter Student Name: ")
+    roll = input("Enter Student Roll No: ")
+    marks = input("Enter Student Marks: ")
+    contact = input("Enter Student Contact No: ")
+    student.append((name, roll, marks, contact))
+
+print('#' * 75)
+print("\nStudent Details:")
+print('#' * 75)
+print("{:15}{:<15}{:<15}{:<15} \n".format("Name","Roll","Marks","Contact"))
+for i in range(len(student)):
+    for j in range(len(student[i])):
+        print(student[i][j] + ' ' * 10 , end='')
+    print('\n')
+print('#' * 75)
---
+++ task_3.py
@@ -0,0 +1,42 @@
+﻿
+def main():
+    print('#' * 75)
+    print("Student Details")
+    print("1. Add Student")
+    print("2. Search Student")
+    print("3. Display Student")
+    print('#' * 75)
+    choice = int(input("Enter your choice: "))
+    if choice == 1:
+        add_student()
+    elif choice == 2:
+        search_student()
+    elif choice == 3:
+        display_student()
+
+
+
+
+main()
+
+student = []
+def add_student():
+    print("Enter the student's Details")
+    n = int(input("Enter the Total number of students: "))
+    for i in range(n):
+        print('#' * 75)
+        student_details = {"Name":input("Enter student Name: "), "Roll":int(input("Enter student Roll no: ")), "Marks":int(input("Enter student Marks: ")), "Contact":input("Enter student Contact No: ")}
+        print()
+        student.append(student_details)
+
+def search_student():
+    pass
+
+def display_student():
+    print("Student Details Updated", student)
+    print('#' * 75)
+    print("{:15}{:<15}{:<15}{:<15} \n".format("Name","Roll","Marks","Contact"))
+    print("*" * 70 +"\n")
+    for stu in student:
+         print("{:15}{:<15}{:<15}{:<15} \n".format(stu['Name'], stu['Roll'],stu['Marks'], stu['Contact']))
+    print('#' * 75)
